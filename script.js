3cec3d45c050d387d47567da6da448a117aac05e
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-03-22 16:36:00 +0100

    Loop over buttons and shopping item within script.js - now toggle and reset button works

---
+++ script.js
@@ -0,0 +1,15 @@
+const btns = document.querySelectorAll('.btns')
+const shoppingItems = document.querySelectorAll('.shopping--item')
+const reset = document.querySelector('.reset')
+
+btns.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        shoppingItems[i].classList.toggle('line--through')
+    })
+})
+
+reset.addEventListener('click', () => {
+    for (let i = 0; i < shoppingItems.length; i++) {
+        shoppingItems[i].classList.remove('line--through')
+    }
+})
