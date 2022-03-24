9410f7c299c4064e5cab21c20a76199e5ee09f4b
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-03-24 11:04:56 +0100

    Trying DOM even to see if it works on iOS

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const btns = document.querySelectorAll('.btns')
-const shoppingItems = document.querySelectorAll('.shopping--item')
-const reset = document.querySelector('.reset')
+window.addEventListener('DOMContentLoaded', () => {
+    const btns = document.querySelectorAll('.btns')
+    const shoppingItems = document.querySelectorAll('.shopping--item')
+    const reset = document.querySelector('.reset')
 
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => {
-        console.log('TEST')
-        shoppingItems[i].classList.toggle('line--through')
+    btns.forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            console.log('TEST')
+            shoppingItems[i].classList.toggle('line--through')
+        })
     })
-})
 
-reset.addEventListener('click', () => {
-    for (let i = 0; i < shoppingItems.length; i++) {
-        shoppingItems[i].classList.remove('line--through')
-    }
+    reset.addEventListener('click', () => {
+        for (let i = 0; i < shoppingItems.length; i++) {
+            shoppingItems[i].classList.remove('line--through')
+        }
+    })
 })
