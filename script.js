3718d8b33dbbfa2cb56c7f0debd9a69f3efe9563
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-03-24 11:38:24 +0100

    now?

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const btns = document.querySelectorAll('.btns')
-    const shoppingItems = document.querySelectorAll('.shopping--item')
-    const reset = document.querySelector('.reset')
+const btns = document.querySelectorAll('.btns')
+const shoppingItems = document.querySelectorAll('.shopping--item')
+const reset = document.querySelector('.reset')
 
-    document.addEventListener('touchstart', () => console.log('touch called'))
+document.addEventListener('touchstart', () => console.log('touch called'))
 
-    btns.forEach((btn, i) => {
-        btn.addEventListener('click', () => {
-            console.log('TEST')
-            shoppingItems[i].classList.toggle('line--through')
-        })
+btns.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        console.log('TEST')
+        shoppingItems[i].classList.toggle('line--through')
     })
+})
 
-    reset.addEventListener('click', () => {
-        for (let i = 0; i < shoppingItems.length; i++) {
-            shoppingItems[i].classList.remove('line--through')
-        }
-    })
+reset.addEventListener('click', () => {
+    for (let i = 0; i < shoppingItems.length; i++) {
+        shoppingItems[i].classList.remove('line--through')
+    }
 })
