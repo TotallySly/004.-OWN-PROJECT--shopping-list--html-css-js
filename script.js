b9bd644df1739c866f6de92e47c21dc101a70143
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-03-24 11:57:59 +0100

    cursor: pointer on spans???

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ const shoppingItems = document.querySelectorAll('.shopping--item')
 const reset = document.querySelector('.reset')
 
 btns.forEach((btn, i) => {
-    btn.addEventListener('click', 'touchscreen', () => {
-        console.log('TEST')
+    btn.addEventListener('click', () => {
         shoppingItems[i].classList.toggle('line--through')
     })
 })
 
-reset.addEventListener('click', () => {
-    for (let i = 0; i < shoppingItems.length; i++) {
-        shoppingItems[i].classList.remove('line--through')
-    }
-})
+// reset.addEventListener('click', () => {
+//     for (let i = 0; i < shoppingItems.length; i++) {
+//         shoppingItems[i].classList.remove('line--through')
+//     }
+// })
