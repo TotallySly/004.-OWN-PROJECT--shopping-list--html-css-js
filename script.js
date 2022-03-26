025a995220340d9ba1b6eac186938c2510eaeb9e
Scott Lyon <82440653+TotallySly@users.noreply.github.com>
2022-03-26 17:22:35 +0100

    Chang buttons to inputs. Testing to see if works on iOS

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const btns = document.querySelectorAll('.btns')
-const shoppingItems = document.querySelectorAll('.shopping--item')
+//const btns = document.querySelectorAll('.btns')
 const reset = document.querySelector('.reset')
+const shoppingItems = document.querySelectorAll('.shopping--list')
+const inputs = document.querySelectorAll('.inputs')
 
-btns.forEach((btn, i) => {
-    btn.addEventListener('click', () => {
+inputs.forEach((input, i) => {
+    input.addEventListener('click', () => {
         shoppingItems[i].classList.toggle('line--through')
     })
 })
+
+//btns.forEach((btn, i) => {
+//   btn.addEventListener('click', () => {
+//       shoppingItems[i].classList.toggle('line--through')
+////   })
+//})
 
 // reset.addEventListener('click', () => {
 //     for (let i = 0; i < shoppingItems.length; i++) {
